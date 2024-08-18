181eaa35adeecdc31911c3f0fd87443f444de991
BabyDell <A.Java.Coder7@gmail.com>
2024-08-17 18:05:22 -0700

    chaged apros

--- src/app/components/background.tsx
+++ src/app/components/background.tsx
@@ -18,7 +18,7 @@ export default function Background() {
         <div className="h-full w-full text-white pt-24 md:pt-40">
           <div className={`text-shadow-xm mb-3 text-sky-900 m-auto text-center font-bold text-3xl sm:text-7xl tracking-wide ${montserrat .className}`}>Maria Guiterrez</div>
           <div className={`text-shadow-xm mb-14 text-sky-900 m-auto text-center text-xl sm:text-3xl tracking-wide ${playFair.className}`}>Century 21 Agent</div>
-          <div className={`text-shadow-sm w-3/4 md:w-1/2 m-auto font-bold text-center pb-10 md:pb-0 text-xl sm:text-6xl md:text-7xl " ${playFair.className}`} >"I'm not a salesperson, I'm a matchmaker."</div>
+          <div className={`text-shadow-sm w-3/4 md:w-1/2 m-auto font-bold text-center pb-10 md:pb-0 text-xl sm:text-6xl md:text-7xl " ${playFair.className}`} >&quot;I&apos;m not a salesperson, I&apos;m a matchmaker.&quot;</div>
         </div>  
       </div>
     </>
