a0cc74bd708e758015204672853cab271f62b64e
BabyDell <A.Java.Coder7@gmail.com>
2024-11-14 12:03:57 -0800

    changed api key location

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -1,16 +1,15 @@
-import { Resend } from 'resend';
-import * as React from 'react';
-import ContactFormEmail from '@/app/components/EmailTemplate';
-
-const resend = new Resend(process.env.RESEND_API_KEY);
+import { Resend } from "resend";
+import * as React from "react";
+import ContactFormEmail from "@/app/components/EmailTemplate";
 
 export async function POST(request: Request) {
   try {
     const { name, email, message } = await request.json();
+    const resend = new Resend(process.env.RESEND_API_KEY);
 
     const { data, error } = await resend.emails.send({
       from: "onboarding@resend.dev",
-      to: ['thatonekid961@gmail.com'],
+      to: ["thatonekid961@gmail.com"],
       subject: `New Contact Form Submission from ${name}`,
       react: ContactFormEmail({ name, email, message }) as React.ReactElement,
     });
